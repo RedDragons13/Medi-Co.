9b0cca45fe2b7a51179e378d69b4b34726ab5b23
bhargavigoud14 <140085517+bhargavigoud14@users.noreply.github.com>
2024-03-26 21:36:31 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { LocationComponent } from './location/location.component';
-import { SignupComponent } from './signup/signup.component'; // Import the SignupComponent
+import { SignupComponent } from './signup/signup.component';
+import { TimeComponent } from './time/time.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'location', component: LocationComponent },
-  { path: 'signup', component: SignupComponent }, // Add the route for SignupComponent
+  { path: 'signup', component: SignupComponent },
+  { path: 'time', component: TimeComponent },
+  // Add the route for SignupComponent
+ 
   // Other routes...
 ];
 
